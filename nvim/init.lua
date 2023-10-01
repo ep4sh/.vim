c6bd7190af3cf1f5054adce97e9910169528b408
Pasha Radchenko <ep4sh2k@gmail.com>
2023-10-01 18:45:53 +0300

    Fix indent-blankline plugin

--- nvim/init.lua
+++ nvim/init.lua
@@ -76,14 +76,6 @@ require('lazy').setup({
     },
   },
 
-  { -- Theme inspired by Atom
-    'navarasu/onedark.nvim',
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme 'onedark'
-    end,
-  },
-
   {
     'catppuccin/nvim',
     priority = 998,
@@ -117,15 +109,7 @@ require('lazy').setup({
     },
   },
 
-  { -- Add indentation guides even on blank lines
-    'lukas-reineke/indent-blankline.nvim',
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help indent_blankline.txt`
-    opts = {
-      char = '┊',
-      show_trailing_blankline_indent = false,
-    },
-  },
+  { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
 
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
